c1ccc2a4c2fd8aebe7daa4ee58dd6f36b75646b6
nileshzarkar <nileshzarkar@gmail.com>
2023-09-12 21:26:34 +0530

    commit

---
+++ loans/src/main/java/com/eazybytes/loans/controller/LoansController.java
@@ -0,0 +1,30 @@
+package com.eazybytes.loans.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eazybytes.loans.model.Customer;
+import com.eazybytes.loans.model.Loans;
+import com.eazybytes.loans.repository.LoansRepository;
+
+@RestController
+public class LoansController {
+    
+    @Autowired
+    private LoansRepository loansRepository;
+
+    @PostMapping("/myLoans")
+    public List<Loans> getLoandDetails(@RequestBody Customer customer) {
+        List<Loans> loans = 
+           loansRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
+           if(loans != null){
+              return loans;
+           } else {
+              return null;
+           }
+    }
+}
