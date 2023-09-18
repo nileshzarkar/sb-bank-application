7e33fc5654695f94ae0d04f8e7a0ac99c78a9df2
nileshzarkar <nileshzarkar@gmail.com>
2023-09-18 23:45:42 +0530

    commit

--- loans/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO loans (customer_id, start_dt, loan_type, total_loan, amount_paid, outstanding_amount, create_dt) 
-VALUES (1,'2020-10-13','Home',200000,50000,150000,'2020-10-13');
-INSERT INTO loans (customer_id, start_dt, loan_type, total_loan, amount_paid, outstanding_amount, create_dt) 
-VALUES (1,'2020-06-06','Vehicle',40000,10000,30000,'2020-06-06');
-INSERT INTO loans (customer_id, start_dt, loan_type, total_loan, amount_paid, outstanding_amount, create_dt) 
-VALUES (1,'2020-02-14','Personal',10000,6000,4000,'2020-02-14');
