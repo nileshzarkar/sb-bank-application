1868fa7c63b0541ad12692e29db2109377aea6ce
nileshzarkar <nileshzarkar@gmail.com>
2023-09-20 21:10:44 +0530

    updated service loans and cards for config changes

--- cards/src/main/java/com/eazybytes/cards/controller/CardsController.java
+++ cards/src/main/java/com/eazybytes/cards/controller/CardsController.java
@@ -3,18 +3,28 @@ package com.eazybytes.cards.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.eazybytes.cards.config.CardsServiceConfig;
 import com.eazybytes.cards.model.Cards;
 import com.eazybytes.cards.model.Customer;
+import com.eazybytes.cards.model.Properties;
 import com.eazybytes.cards.repository.CardsRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 @RestController
 public class CardsController {
+
 	@Autowired
 	private CardsRepository cardsRepository;
+
+	@Autowired
+	private CardsServiceConfig cardsServiceConfig;
 
 	@PostMapping("/myCards")
 	public List<Cards> getCardDetails(@RequestBody Customer customer) {
@@ -24,5 +34,14 @@ public class CardsController {
 		} else {
 			return null;
 		}
+	}
+
+	@GetMapping("/cards/properties")
+	public String getPropertyDetails() throws JsonProcessingException {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties(cardsServiceConfig.getMsg(), cardsServiceConfig.getBuildVersion(),
+				cardsServiceConfig.getMailDetails(), cardsServiceConfig.getActiveBranches());
+		String jsonStr = ow.writeValueAsString(properties);
+		return jsonStr;
 	}
 }
