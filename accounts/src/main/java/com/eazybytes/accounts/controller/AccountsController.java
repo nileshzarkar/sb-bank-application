6121972491593da2def075c5d5b5625e4ebfa580
nileshzarkar <nileshzarkar@gmail.com>
2023-09-23 08:05:43 +0530

    updated accounts microservice for feign client

--- accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -1,5 +1,7 @@
 package com.eazybytes.accounts.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,7 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.eazybytes.accounts.config.AccountsServiceConfig;
 import com.eazybytes.accounts.model.Accounts;
+import com.eazybytes.accounts.model.Cards;
 import com.eazybytes.accounts.model.Customer;
+import com.eazybytes.accounts.model.CustomerDetails;
+import com.eazybytes.accounts.model.Loans;
 import com.eazybytes.accounts.model.Properties;
 import com.eazybytes.accounts.repository.AccountsRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -22,6 +27,12 @@ public class AccountsController {
 
 	@Autowired
 	AccountsServiceConfig accountsConfig;
+
+	@Autowired
+	LoansFeignClient loansFeignClient;
+	@Autowired
+	CardsFeignClient cardsFeignClient;
+
 
 	@PostMapping("/myAccount")
 	public Accounts getAccountDetails(@RequestBody Customer customer) {
@@ -41,5 +52,20 @@ public class AccountsController {
 		String jsonStr = ow.writeValueAsString(properties);
 		return jsonStr;
 	}
+
+	@PostMapping("/myCustomerDetails")
+	public CustomerDetails myCustomerDetails(@RequestBody Customer customer) {
+		Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
+		List<Loans> loans = loansFeignClient.getLoansDetails(customer);
+		List<Cards> cards = cardsFeignClient.getCardDetails(customer);
+
+		CustomerDetails customerDetails = new CustomerDetails();
+		customerDetails.setAccounts(accounts);
+		customerDetails.setLoans(loans);
+		customerDetails.setCards(cards);
+		
+		return customerDetails;
+	}
+
 
 }
---
+++ accounts/src/main/java/com/eazybytes/accounts/model/Cards.java
@@ -0,0 +1,20 @@
+package com.eazybytes.accounts.model;
+
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter  @Setter @ToString
+public class Cards {
+	private int cardId;
+	private int customerId;
+	private String cardNumber;
+	private String cardType;
+	private int totalLimit;
+	private int amountUsed;
+	private int availableAmount;
+	private Date createDt;
+}
+
